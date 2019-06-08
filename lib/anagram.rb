6a28f51ab04bb024acd90b9752032c2fb497e65a
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-08 15:08:57 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
+require 'pry' 
+
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(array)
+  array.map do |anagram|
+  new = anagram.split("").sort
+  new_word = @word.split.sort
+  
+  
+  binding.pry
+   end 
+  end
+  
+end

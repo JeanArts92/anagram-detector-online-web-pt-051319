dddfa2fff4581874328256a73e6d2b2579c37045
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-08 15:31:23 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
   
   def match(array)
     new = @word.split("").sort
-    last = array.collect{|anagram| anagram.split("").sort == new}
+    last = array.select{|anagram| anagram.split("").sort == new}
   #new = anagram.split("").sort
   #new_word = @word.split.sort
   
